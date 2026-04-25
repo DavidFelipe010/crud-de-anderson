d49b5f6c3bbdf968d084cac424f13fa63aa7f74a
David Felipe <david.dfcsb9@gmail.com>
2026-04-25 20:57:07 -0300

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,17 +1,93 @@
 from modulos.cadastrar import cadastrar_funcionario, cadastrar_cliente, cadastrar_produto
 from modulos.listar import listar_funcionario, listar_cliente, listar_produto
 from modulos.alterar import menu_alterar_funcionarios, menu_alterar_cliente, menu_alterar_produtos
-def main():
-   nome = []
-   cpf = []
-   salario = []
-   
-   nome_cliente = []
-   cpf_cliente = []
-   endereco = []
+from modulos.excluir import excluir_funcionario, excluir_cliente, excluir_produto
+from modulos.totais import total_funcionarios, total_clientes, total_produtos
+from modulos.verficadores import verificar_nums_naturais
+from modulos.texto import menu, limpar_tela, linha
 
-   nome_produto = []
-   preco_produto = []
+def main():
+   nome_funcionarios = []
+   cpf_funcionarios = []
+   salario_funcionarios = []
+   
+   nome_clientes = []
+   cpf_clientes = []
+   endereco_clientes = []
+
+   nome_produtos = []
+   preco_produtos = []
+
+   while True:
+      limpar_tela()
+      txt = 'Menu de Opções'
+
+      print(f'''{"=" * (len(txt) + 16):^100}
+      {txt:^88}
+{"=" * (len(txt) + 16):^100}''')
+
+      linha(100)
+      menu()
+      linha(100)
+
+      esc = verificar_nums_naturais('Sua escolha: ')
+
+      match esc:
+         case 0:
+            limpar_tela()
+            print('\033[32mObrigado por utilizar nosso CRUD! Volte sempre!\033[m')
+            break
+
+         case 1:
+            cadastrar_funcionario(nome_funcionarios, cpf_funcionarios, salario_funcionarios)
+
+         case 2:
+            listar_funcionario(nome_funcionarios, cpf_funcionarios, salario_funcionarios)
+
+         case 3:
+            menu_alterar_funcionarios(nome_funcionarios, cpf_funcionarios, salario_funcionarios)
+
+         case 4:
+            excluir_funcionario(nome_funcionarios, cpf_funcionarios, salario_funcionarios)
+
+         case 5:
+            cadastrar_cliente(nome_clientes, cpf_clientes, endereco_clientes)
+
+         case 6:
+            listar_cliente(nome_clientes, cpf_clientes, endereco_clientes)
+
+         case 7:
+            menu_alterar_cliente(nome_clientes, cpf_clientes, endereco_clientes)
+
+         case 8:
+            excluir_cliente(nome_clientes, cpf_clientes, endereco_clientes)
+
+         case 9:
+            cadastrar_produto(nome_produtos, preco_produtos)
+
+         case 10:
+            listar_produto(nome_produtos, preco_produtos)
+
+         case 11:
+            menu_alterar_produtos(nome_produtos, preco_produtos)
+
+         case 12:
+            excluir_produto(nome_produtos, preco_produtos)
+
+         case 13:
+            total_funcionarios(nome_funcionarios)
+
+         case 14:
+            total_clientes(nome_funcionarios)
+
+         case 15:
+            total_produtos(nome_produtos)
+
+         case _:
+            limpar_tela()
+            print(f'\033[31m[ERROR] Escolha inexistente! Escolha um valor entre \033[33m[0-15]\033[31m!\033[m')
+            input('\nPressione ENTER para continuar...')
+
 
 if __name__ == '__main__':
     main()
--- modulos/alterar/__init__.py
+++ modulos/alterar/__init__.py
@@ -10,27 +10,34 @@ from modulos.verficadores import verificar_nums_naturais, verificar_nome, verifi
 def menu_alterar_funcionarios(lst_nome, lst_cpf, lst_salario):
     limpar_tela()
     titulo("Alterar dados de funcionarios")
-    lista_funcinario(lst_nome, lst_cpf, lst_salario)
 
-    while True:
-        opcao = verificar_nums_naturais('\nAltera:  nome(1) | CPF(2) | endereco(3) | Sair(0): ')
-        
-        match opcao:
-            case 0:
-                input('\nPressione ENTER para continuar...')
-                return True
-            case 1:
-                if alterar_nome_funcionarios(lst_nome, lst_cpf, lst_salario):
-                    return True 
-            case 2:
-                if alterar_cpf_funcionarios(lst_nome, lst_cpf, lst_salario):
+    if not lst_nome or not lst_cpf or not lst_salario:
+        print(f'\033[31m[ERROR] Não há nenhum funcionário cadastrado!\033[m')
+        input('Pressione ENTER para continuar...')
+        return True
+
+    else:
+        lista_funcinario(lst_nome, lst_cpf, lst_salario)
+
+        while True:
+            opcao = verificar_nums_naturais('\nAltera:  nome(1) | CPF(2) | salario(3) | Sair(0): ')
+
+            match opcao:
+                case 0:
+                    input('\nPressione ENTER para continuar...')
                     return True
-            case 3:
-                if alterar_salario_funcionarios(lst_nome, lst_cpf, lst_salario):
-                    return True 
-            case _:
-                print('\033[31m[ERROR] Escolha uma opção válida (1-3).\033[m')
-                input('\nPressione ENTER para continuar...')
+                case 1:
+                    if alterar_nome_funcionarios(lst_nome, lst_cpf, lst_salario):
+                        return True
+                case 2:
+                    if alterar_cpf_funcionarios(lst_nome, lst_cpf, lst_salario):
+                        return True
+                case 3:
+                    if alterar_salario_funcionarios(lst_nome, lst_cpf, lst_salario):
+                        return True
+                case _:
+                    print('\033[31m[ERROR] Escolha uma opção válida (1-3).\033[m')
+                    input('\nPressione ENTER para continuar...')
 
 
 def alterar_nome_funcionarios(lst_nome, lst_cpf, lst_salario):
@@ -39,18 +46,19 @@ def alterar_nome_funcionarios(lst_nome, lst_cpf, lst_salario):
         limpar_tela()
         titulo("Alterar dados de funcionarios")
 
+
         print("Alterar Nome".title().center(50))
         linha()
 
         lista_funcinario(lst_nome, lst_cpf, lst_salario)
-        
+
         id_escolhido = verificar_nums_naturais('\nDigite o ID (0=cancelar): ')
-        
+
         if id_escolhido == 0:
             print("Cancelado")
             input("\nPressione ENTER para continuar...")
             return False
-        
+
         if 1 <= id_escolhido <= len(lst_nome):
             indice = id_escolhido - 1
             novo_nome = verificar_nome('Digite o novo nome: ')
@@ -81,7 +89,7 @@ def alterar_cpf_funcionarios(lst_nome, lst_cpf, lst_salario):
             print("Cancelado")
             input('\nPressione ENTER para continuar...')
             return False
-        
+
         if 1 <= id_escolhido <= len(lst_cpf):
             indice = id_escolhido - 1
             novo_cpf = verificar_cpf("Digite o novo CPF: ")
@@ -89,7 +97,7 @@ def alterar_cpf_funcionarios(lst_nome, lst_cpf, lst_salario):
 
             print(f'\033[32m[Sucesso] CPF alterado\033[m')
             return True
-        
+
         else:
             print(f'\033[31m[ERROR] ID {id_escolhido} não existe!\033[m')
             input('\nPressione ENTER para continuar...')
@@ -110,7 +118,7 @@ def alterar_salario_funcionarios(lst_nome, lst_cpf, lst_salario):
             print('Cancelado')
             input('\nPressione ENTER para continuar...')
             return False
-        
+
         if 1 <= id_escolhido <= len(lst_salario):
             indice = id_escolhido - 1
             novo_salario = verificar_salario('Digite o novo salario: ')
@@ -118,39 +126,44 @@ def alterar_salario_funcionarios(lst_nome, lst_cpf, lst_salario):
 
             print(f'\033[32m[Sucesso] endereco alterado\033[m')
             return True
-        
+
         else:
             print(f'\033[31m[ERROR] ID {id_escolhido} não existe!\033[m')
             input('\nPressione ENTER para continuar...')
 
 
-
-
-
 def menu_alterar_cliente(lst_nome, lst_cpf, lst_endereco):
     limpar_tela()
     titulo("Alterar dados de cliente")
-    lista_cliente(lst_nome, lst_cpf, lst_endereco)
 
-    while True:
-        opcao = verificar_nums_naturais('\nAltera:  nome(1) | CPF(2) | endereco(3) | Sair(0): ')
-        
-        match opcao:
-            case 0:
-                input('\nPressione ENTER para continuar...')
-                return True
-            case 1:
-                if alterar_nome_clientes(lst_nome, lst_cpf, lst_endereco):
-                    return True 
-            case 2:
-                if alterar_cpf_clientes(lst_nome, lst_cpf, lst_endereco):
+    if not lst_nome or not lst_cpf or not lst_endereco:
+        print(f'\033[31m[ERROR] Não há nenhum cliente cadastrado!\033[m')
+        input('Pressione ENTER para continuar...')
+        return True
+
+    else:
+
+        lista_cliente(lst_nome, lst_cpf, lst_endereco)
+
+        while True:
+            opcao = verificar_nums_naturais('\nAltera:  nome(1) | CPF(2) | endereco(3) | Sair(0): ')
+
+            match opcao:
+                case 0:
+                    input('\nPressione ENTER para continuar...')
                     return True
-            case 3:
-                if alterar_endereco_clientes(lst_nome, lst_cpf, lst_endereco):
-                    return True 
-            case _:
-                print('\033[31m[ERROR] Escolha uma opção válida (1-3).\033[m')
-                input('\nPressione ENTER para continuar...')
+                case 1:
+                    if alterar_nome_clientes(lst_nome, lst_cpf, lst_endereco):
+                        return True
+                case 2:
+                    if alterar_cpf_clientes(lst_nome, lst_cpf, lst_endereco):
+                        return True
+                case 3:
+                    if alterar_endereco_clientes(lst_nome, lst_cpf, lst_endereco):
+                        return True
+                case _:
+                    print('\033[31m[ERROR] Escolha uma opção válida (1-3).\033[m')
+                    input('\nPressione ENTER para continuar...')
 
 
 def alterar_nome_clientes(lst_nome, lst_cpf, lst_endereco):
@@ -232,45 +245,114 @@ def alterar_endereco_clientes(lst_nome, lst_cpf, lst_endereco):
             print("Cancelado")
             input('\nPressione ENTER para continuar...')
             return False
-        
+
         if 1 <= id_escolhido <= len(lst_endereco):
             indice = id_escolhido - 1
-            novo_endereco = verificar_endereco("Digite o novo endereço: ")
-            lst_cpf[indice] = novo_endereco
 
-            print(f'\033[32m[Sucesso] Endereço alterado\033[m')
-            return True
-        
+
         else:
             print(f'\033[31m[ERROR] ID {id_escolhido} não existe!\033[m')
             input('\nPressione ENTER para continuar...')
 
+        opcao = verificar_nums_naturais('\nQual dado de enderço você quer alterar:  número da casa(1) | rua(2) | bairro(3) | cidade(4) | estado(5) | complemento(6) |Sair(0): ')
 
+        match opcao:
+            case 0:
+                limpar_tela()
+                input('\033[31mOperação cancelada com sucesso!\033[m\n')
+                input('Pressione ENTER para continuar...')
+                return False
 
+            case 1:
+                limpar_tela()
+                lst_endereco[indice][0] = verificar_nums_naturais('Novo número da casa: ')
+
+                limpar_tela()
+                print('\033[32mNúmero da casa alterada com sucesso!\033[m')
+                input('Pressione ENTER para continuar...')
+                return True
+
+            case 2:
+                limpar_tela()
+                lst_endereco[indice][1] = input('Nova rua: ').strip().lower()
+
+                limpar_tela()
+                print('\033[32mRua alterada com sucesso!\033[m')
+                input('Pressione ENTER para continuar...')
+                return True
+
+            case 3:
+                limpar_tela()
+                lst_endereco[indice][2] = input('Novo bairro: ').strip().lower()
+
+                limpar_tela()
+                print('\033[32mBairro alterado com sucesso!\033[m')
+                input('Pressione ENTER para continuar...')
+                return True
+
+            case 4:
+                limpar_tela()
+                lst_endereco[indice][3] = input('Nova cidade: ').strip().lower()
+
+                limpar_tela()
+                print('\033[32mCidade alterada com sucesso!\033[m')
+                input('Pressione ENTER para continuar...')
+                return True
+
+            case 5:
+                limpar_tela()
+                lst_endereco[indice][4] = input('Nova estado: ').strip().lower()
+
+                limpar_tela()
+                print('\033[32mEstado alterado com sucesso!\033[m')
+                input('Pressione ENTER para continuar...')
+                return True
+
+            case 6:
+                limpar_tela()
+                lst_endereco[indice][1] = input('Novo complemento: ').strip().lower()
+
+                limpar_tela()
+                print('\033[32mComplemento alterado com sucesso!\033[m')
+                input('Pressione ENTER para continuar...')
+                return True
+
+            case _:
+                limpar_tela()
+                print('\033[31m[ERROR] Opção invalida! Escolha um opção entre \033[33m[0-6]\033[31m!\033[m\n')
+                input('Pressione ENTER para continuar...')
+                continue
 
 
 def menu_alterar_produtos(lst_nome, lst_preco):
 
     limpar_tela()
     titulo("Alterar dados de produtos")
-    lista_funcinario(lst_nome, lst_preco)
 
-    while True:
-        opcao = verificar_nums_naturais('\nAltera: Nome(1) | Preço(2) | Sair(0): ')
-        
-        match opcao:
-            case 0:
-                input('\nPressione ENTER para continuar...')
-                return True
-            case 1:
-                if alterar_nome_produtos(lst_nome, lst_preco):
-                    return True 
-            case 2:
-                if alterar_preco_produtos(lst_nome, lst_preco):
+    if not lst_nome or not lst_preco:
+        print(f'\033[31m[ERROR] Não há nenhum produto cadastrado!\033[m')
+        input('Pressione ENTER para continuar...')
+        return True
+
+    else:
+        lista_produto(lst_nome, lst_preco)
+
+        while True:
+            opcao = verificar_nums_naturais('\nAltera: Nome(1) | Preço(2) | Sair(0): ')
+
+            match opcao:
+                case 0:
+                    input('\nPressione ENTER para continuar...')
                     return True
-            case _:
-                print('\033[31m[ERROR] Escolha uma opção válida (1-3).\033[m')
-                input('\nPressione ENTER para continuar...')
+                case 1:
+                    if alterar_nome_produtos(lst_nome, lst_preco):
+                        return True
+                case 2:
+                    if alterar_preco_produtos(lst_nome, lst_preco):
+                        return True
+                case _:
+                    print('\033[31m[ERROR] Escolha uma opção válida (1-3).\033[m')
+                    input('\nPressione ENTER para continuar...')
 
 
 def alterar_nome_produtos(lst_nome, lst_preco):
