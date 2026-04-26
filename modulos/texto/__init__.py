ef17971a41637f8a12c4730f8df8126a36092647
David Felipe <david.dfcsb9@gmail.com>
2026-04-25 21:09:38 -0300

    Add files via upload

--- modulos/texto/__init__.py
+++ modulos/texto/__init__.py
@@ -17,15 +17,15 @@ def linha(qtd=80):
 
 
 def menu():
-    print(f'''\033[31m[0] - Sair\033[m                                  \033[32m[1] - Cadastrar funcionário
-[2] - Listar funcionários                   [3] - Alterar funcionário
-[4] - Excluir funcionário\033[m                   \033[33m[5] - Cadastrar cliente     
-[6] - Listar clientes                       [7] - Alterar cliente       
-[8] - Excluir cliente\033[m                       \033[34m[9] - Cadastrar produtos       
-[10] - Listar produtos                    [11] - Alterar produto
+    print(f'''\033[31m[0] - Sair\033[m                                          \033[32m[1] - Cadastrar funcionário
+[2] - Listar funcionários                           [3] - Alterar funcionário
+[4] - Excluir funcionário\033[m                           \033[33m[5] - Cadastrar cliente     
+[6] - Listar clientes                               [7] - Alterar cliente       
+[8] - Excluir cliente\033[m                               \033[34m[9] - Cadastrar produtos       
+[10] - Listar produtos                              [11] - Alterar produto
 [12] - Excluir produto\033[m                      
 {"=" * 100}
-\033[32m[13] - Ver o total de funcionários cadastrados\033[m \033[33m[14] - Ver o total de clientes cadastrados\033[m  
+\033[32m[13] - Ver o total de funcionários cadastrados\033[m    \033[33m[14] - Ver o total de clientes cadastrados\033[m  
 \033[34m[15] - Ver o total de produtos cadastrados\033[m''')
 
 def formatar_cpf(cpf: str) -> str:
